4bc94934c65ee6d373b4ba5fb97c25975f21619f
Kent Fredric <kentfredric@gmail.com>
2014-10-07 05:33:14 +1300

    Build results of c4a4586 (on master)

--- lib/Gentoo/Overlay/Category.pm
+++ lib/Gentoo/Overlay/Category.pm
@@ -40,7 +40,7 @@ use MooseX::Has::Sugar qw( ro required coerce lazy lazy_build );
 use MooseX::Types::Moose qw( HashRef Str );
 use MooseX::Types::Path::Tiny qw( File Dir Path );
 use MooseX::ClassAttribute qw( class_has );
-use Gentoo::Overlay::Types qw( Gentoo__Overlay_CategoryName Gentoo__Overlay_Package );
+use Gentoo::Overlay::Types qw( Gentoo__Overlay_CategoryName Gentoo__Overlay_Package Gentoo__Overlay_Overlay );
 use Gentoo::Overlay::Exceptions qw( exception );
 use namespace::autoclean;
 
